4873c329be234e27c644602e2077229536b45933
truptilangalia-crest <79827058+truptilangalia-crest@users.noreply.github.com>
2022-04-05 15:33:02 +0530

    Update enrich_detections.py

--- security_content_automation/enrich_detections.py
+++ security_content_automation/enrich_detections.py
@@ -14,7 +14,7 @@ import git
 import yaml
 from github import Github
 
-FORMAT4 = ""
+FORMAT = "45"
 TIMESTAMP_FORMAT = '%(asctime)s %(levelname)s - %(message)s'
 
 def fetch_ta_cim_mapping_report(file_name):
